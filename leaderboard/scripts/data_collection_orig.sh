dfd647e321e6f6ecf4e183e9d4b94c910bf65c07
Michael Foster <m.foster@sheffield.ac.uk>
2023-03-03 10:49:45 +0000

    Running Interfuser routes

---
+++ leaderboard/scripts/data_collection_orig.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+export CARLA_ROOT=/home/michael/Documents/CITCoM/carla/CARLA
+export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
+export PYTHONPATH=$PYTHONPATH:leaderboard
+export PYTHONPATH=$PYTHONPATH:leaderboard/team_code
+export PYTHONPATH=$PYTHONPATH:scenario_runner
+
+export LEADERBOARD_ROOT=leaderboard
+export CHALLENGE_TRACK_CODENAME=SENSORS
+export PORT=2000
+export TM_PORT=8000
+export DEBUG_CHALLENGE=0
+export REPETITIONS=1 # multiple evaluation runs
+export RESUME=True
+export DATA_COLLECTION=True
+
+
+# Roach data collection
+export ROUTES=leaderboard/data/training_routes/weather_routes_town01_short.xml
+export TEAM_AGENT=team_code/roach_ap_agent.py
+export TEAM_CONFIG=roach/config/config_agent.yaml
+export CHECKPOINT_ENDPOINT=data/data_interfuser_town01_results/results.json
+export SCENARIOS=leaderboard/data/scenarios/all_towns_traffic_scenarios.json
+export SAVE_PATH=data/data_interfuser_town01_results/routes/
+
+
+
+python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
+--scenarios=${SCENARIOS}  \
+--routes=${ROUTES} \
+--repetitions=${REPETITIONS} \
+--track=${CHALLENGE_TRACK_CODENAME} \
+--checkpoint=${CHECKPOINT_ENDPOINT} \
+--agent=${TEAM_AGENT} \
+--agent-config=${TEAM_CONFIG} \
+--debug=${DEBUG_CHALLENGE} \
+--record=${RECORD_PATH} \
+--resume=${RESUME} \
+--port=${PORT} \
+--trafficManagerPort=${TM_PORT}
+
+
