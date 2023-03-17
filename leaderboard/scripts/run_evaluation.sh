951f7856a6ae5e7a20feb750399c23357d4df6d2
Michael Foster <m.foster@sheffield.ac.uk>
2023-03-17 09:58:02 +0000

    Changed eval routes to training routes

--- leaderboard/scripts/run_evaluation.sh
+++ leaderboard/scripts/run_evaluation.sh
@@ -19,9 +19,9 @@ export RESUME=True
 
 # TCP evaluation
 # export ROUTES=leaderboard/data/evaluation_routes/routes_lav_valid.xml
-export ROUTES=leaderboard/data/CITCoM_routes/routes_town01.xml
+export ROUTES=leaderboard/data/TCP_training_routes/routes_town01.xml
 export TEAM_AGENT=team_code/tcp_agent.py
-export TEAM_CONFIG="log/TCP/epoch=3-last.ckpt"
+export TEAM_CONFIG="epoch=59-last.ckpt"
 export SCENARIOS=leaderboard/data/scenarios/all_towns_traffic_scenarios.json
 export SAVE_PATH=data/results_TCP/
 export CHECKPOINT_ENDPOINT=${SAVE_PATH}/results_TCP.json
