1f93b67cc5cc86a38fa810703fb1c856e487b2de
Michael Foster <m.foster@sheffield.ac.uk>
2023-04-11 12:43:47 +0100

    iv table

--- instrumental_variables.py
+++ instrumental_variables.py
@@ -36,6 +36,25 @@ inputs = [
 ]
 
 
+def calculate_coefficients(df_original, df_modified):
+    total_effect_modified = sm.OLS(
+        df_modified["duration_system"], df_modified[[ip, "Intercept"]]
+    ).fit()
+    total_effect_original = sm.OLS(
+        df_original["duration_system"], df_original[[ip, "Intercept"]]
+    ).fit()
+
+    # calculate ip -> game time
+    direct_effect_modified = sm.OLS(
+        df_modified["duration_game"], df_modified[[ip, "Intercept"]]
+    ).fit()
+    direct_effect_original = sm.OLS(
+        df_original["duration_game"], df_original[[ip, "Intercept"]]
+    ).fit()
+    
+    return (total_effect_original.params[ip] / direct_effect_original.params[ip],
+        total_effect_modified.params[ip] / direct_effect_modified.params[ip])
+
 df_modified = pd.read_csv(v1_datapath, index_col=0)
 df_modified["Intercept"] = 1
 df_original = pd.read_csv(v2_datapath, index_col=0)
@@ -43,40 +62,44 @@ df_original["Intercept"] = 1
 df_original["number_of_drivers"] = 120
 df_original["number_of_walkers"] = 0
 
+# using df_modified[[ip]] to calculate direct_effect_original is NOT a bug because the
+# routes have the same weather conditions
+for ip in inputs:
+    df_original[ip] = df_modified[ip]
+
 data = {}
+BOOTSTRAPS = 100
 
 # Try each input as an instrument
 for ip in inputs:
     print(ip, len(set(df_modified[ip])))
-    # Calculate ip -> system time
-    # using df_modified[[ip]] to calculate total_effect_original is NOT a bug because the
-    # routes have the same weather conditions
-    total_effect_modified = sm.OLS(
-        df_modified["duration_system"], df_modified[[ip, "Intercept"]]
-    ).fit()
-    total_effect_original = sm.OLS(
-        df_original["duration_system"], df_modified[[ip, "Intercept"]]
-    ).fit()
-
-    # calculate ip -> game time
-    # using df_modified[[ip]] to calculate direct_effect_original is NOT a bug because the
-    # routes have the same weather conditions
-    direct_effect_modified = sm.OLS(
-        df_modified["duration_game"], df_modified[[ip, "Intercept"]]
-    ).fit()
-    direct_effect_original = sm.OLS(
-        df_original["duration_game"], df_modified[[ip, "Intercept"]]
-    ).fit()
+    effect_original, effect_modified = calculate_coefficients(df_original, df_modified)
     
-    print("  original", total_effect_original.params[ip], direct_effect_original.params[ip])
-    print("  modified", total_effect_modified.params[ip], direct_effect_modified.params[ip])
-
-    # We do not want numbers of walkers and drivers for the original version since these were always the same
-    data[ip.replace("_", "\_")] = [
-        len(set(df_modified[ip])),
-        str(round(total_effect_original.params[ip] / direct_effect_original.params[ip], 2)),
-        str(round(total_effect_modified.params[ip] / direct_effect_modified.params[ip], 2)),
-    ]
+    bootstrap_samples = [
+                calculate_coefficients(df_original.sample(len(df_original), replace=True), df_modified.sample(len(df_modified), replace=True))
+                for _ in range(BOOTSTRAPS)
+            ]
+    original, modified = zip(*bootstrap_samples)
+    
+    original = sorted(list(original))
+    modified = sorted(list(modified))
+    
+    bound = int((BOOTSTRAPS * 0.05) / 2)
+    modified_low = modified[bound]
+    modified_high = modified[BOOTSTRAPS - bound]
+    original_low = original[bound]
+    original_high = original[BOOTSTRAPS - bound]
+    
+    data[ip.replace("_", "\_")] = {
+        "datapoints": len(set(df_modified[ip])),
+        "effect modified": str(round(effect_modified, 2)),
+        "modified low": str(round(modified_low, 2)),
+        "modified high": str(round(modified_high, 2)),
+        "effect original": str(round(effect_original, 2)),
+        "original low": str(round(original_low, 2)),
+        "original high": str(round(original_high, 2)),
+    }
+    
 
 adjusted_modified = sm.OLS(
     df_modified["duration_system"],
@@ -102,17 +125,22 @@ adjusted_original = sm.OLS(
 ).fit()
 
 
-data["Classical adjustment"] = [
-    "",
-    f"{round(adjusted_modified.params['duration_game'], 2)} {adjusted_modified.conf_int().loc['duration_game'].round(2).to_list()}",
-    f"{round(adjusted_original.params['duration_game'], 2)} {adjusted_original.conf_int().loc['duration_game'].round(2).to_list()}",
-]
+data["Classical adjustment"] = {
+    "datapoints": None,
+    "effect modified": round(adjusted_modified.params['duration_game'], 2), 
+    "modified low": float(adjusted_modified.conf_int().loc['duration_game'].round(2)[0]),
+    "modified high": float(adjusted_modified.conf_int().loc['duration_game'].round(2)[1]),
+    "effect original": round(adjusted_original.params['duration_game'], 2),
+    "original low": float(adjusted_original.conf_int().loc['duration_game'].round(2)[0]),
+    "original high": float(adjusted_original.conf_int().loc['duration_game'].round(2)[1]),
+}
 
 
 
 data = (
     pd.DataFrame(data)
     .transpose()
-    .rename({0: "No. points", 1: "Original", 2: "Modified"}, axis=1)
 )
+data["effect modified"] = data['effect modified'].astype(float)
+print(data)
 print(data.style.to_latex())
