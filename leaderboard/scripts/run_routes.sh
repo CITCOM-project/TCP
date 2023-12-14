f86c434525de2e3bca0ab33afa3c8fa647e3b970
Michael Foster <m.foster@sheffield.ac.uk>
2023-12-14 17:02:45 +0000

    proper indexing

--- leaderboard/scripts/run_routes.sh
+++ leaderboard/scripts/run_routes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {0..299}
 do
-   bash leaderboard/scripts/data_collection.sh -v vehicle.bmw.isetta -c 11 -r 1
+   bash leaderboard/scripts/data_collection.sh -v vehicle.bmw.isetta -c 11 -r $i
 done
