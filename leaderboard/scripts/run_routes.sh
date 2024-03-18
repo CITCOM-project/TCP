7714be5aabf5c6a4d5d24f29756b53809dce61dc
Michael Foster <m.foster@sheffield.ac.uk>
2024-03-18 10:33:54 +0000

    run routes parameterised

--- leaderboard/scripts/run_routes.sh
+++ leaderboard/scripts/run_routes.sh
@@ -2,6 +2,6 @@
 # @author: michael
 for i in {0..299}
 do
-   bash leaderboard/scripts/data_collection.sh -v vehicle.bmw.isetta -c 11 -r $i
-   bash leaderboard/scripts/data_collection.sh -v vehicle.lincoln.mkz2017 -c 11 -r $i
+   bash leaderboard/scripts/data_collection.sh -v vehicle.bmw.isetta -c $1 -r $i
+   bash leaderboard/scripts/data_collection.sh -v vehicle.lincoln.mkz2017 -c $1 -r $i
 done
